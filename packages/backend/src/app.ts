0f90a45bd0e805b901642db13c88c7d567f4fa4f
Erik Hellman <erik.hellman@iteam.se>
2022-09-22 11:41:59 +0200

    updated with file storage

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -2,15 +2,29 @@ import TodoItem from "@my-todo-app/shared";
 import cors from "cors";
 import express, { Application, json, Request, Response } from "express";
 import crypto from "crypto"
+import dotenv from 'dotenv'
+import {readFile, writeFile} from 'fs'
+
+dotenv.config()
 
 const app: Application = express();
 app.use(cors()); // TODO Configure CORS properly to make the app secure.
 app.use(json());
 const port: number = parseInt(process.env.SERVER_PORT || "3001");
+const TODO_FILE = process.env.TODO_FILE || "todos.json"
+let TODO_ITEMS: TodoItem[] = [];
 
-const TODO_ITEMS: TodoItem[] = [
-  { id: crypto.randomUUID(), text: "Städa kontoret!", timeStamp: new Date() },
-];
+readFile(TODO_FILE, (err, data) => {
+  if (err) throw err
+  TODO_ITEMS = JSON.parse(data.toString()) as unknown as TodoItem[]
+  console.log('Loaded todo items:', TODO_ITEMS)
+})
+
+function writeTodosToFile(todoItems:TodoItem[]) {
+  writeFile(TODO_FILE, JSON.stringify(todoItems), (err) => {
+    console.error('Error writing todos to file!', err)
+  })
+}
 
 app.get("/todos", (req: Request, res: Response<TodoItem[]>) => {
   res.send(TODO_ITEMS);
@@ -21,6 +35,7 @@ app.post("/todos", (req: Request<TodoItem>, res: Response<TodoItem[]>) => {
   todoItem.id = crypto.randomUUID()
   console.log('Got new todo item:', todoItem)
   TODO_ITEMS.push(todoItem)
+  writeTodosToFile(TODO_ITEMS)
   res.send(TODO_ITEMS)
 });
 
